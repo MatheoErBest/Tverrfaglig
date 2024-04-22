68feac81e05764c6b72ab2942d2167fc0d9cdaae
“MatheoErBest” <matheo.roen@gmail.com>
2024-04-22 09:27:06 +0200

    sprites

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
-import pymysql
 import pygame
-import random
 
 # Initialiserer pygame
 pygame.init()
 
 # Definerer vindusstørrelsen
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 1920
+WINDOW_HEIGHT = 1080
 
-# Lager vinduet til programet
+# Lager vinduet til programmet
 win = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 # Farger
 WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+RED = (255, 0, 0)  # Fargen til firkanten
 
-# Setter opp hoved løøka til spillet
+# Setter firkantens posisjon og størrelse
+square_x = 100
+square_y = 100
+square_width = 20
+square_height = 20
+
+# Setter opp hovedløkken til spillet
 running = True
 while running:
     # Hører etter hendelser
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        if event.type == pygame.KEYDOWN:
+            pass
 
     # Tegner bakgrunnen
     win.fill(WHITE)
+
+    # Tegner firkanten
+    pygame.draw.rect(win, RED, (square_x, square_y, square_width, square_height))
 
     # Oppdaterer vinduet
     pygame.display.update()
